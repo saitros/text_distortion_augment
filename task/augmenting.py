47f97408f5e3f4b92329dfa95feba4ae87fc2ed9
fhzh123 <fhzh@naver.com>
2023-02-14 00:51:17 +0900

    fix

--- task/augmenting.py
+++ task/augmenting.py
@@ -59,7 +59,7 @@ def augmenting(args):
             train_src_token_type_ids = f.get('train_src_token_type_ids')[:]
         else:
             train_src_token_type_ids = list()
-        
+
     with open(os.path.join(save_path, 'word2id.pkl'), 'rb') as f:
         data_ = pickle.load(f)
         src_word2id = data_['src_word2id']
@@ -76,8 +76,8 @@ def augmenting(args):
 
     # 1) Model initiating
     write_log(logger, 'Instantiating model...')
-    model = TransformerModel(model_type=args.model_type, src_max_len=args.src_max_len, isPreTrain=args.isPreTrain, 
-                             num_labels=num_labels, dropout=args.dropout)
+    model = TransformerModel(model_type=args.model_type, src_max_len=args.src_max_len,
+                                 isPreTrain=args.isPreTrain, num_labels=num_labels, dropout=args.dropout)
     model.to(device)
 
     # 2) Dataloader setting
@@ -91,7 +91,7 @@ def augmenting(args):
                             batch_size=args.batch_size, shuffle=False, pin_memory=True,
                             num_workers=args.num_workers)
     }
-    write_log(logger, f"Total number of trainingsets  iterations - {len(dataset_dict['train'])}, {len(dataloader_dict['train'])}")
+    write_log(logger, f"Total number of trainingsets iterations - {len(dataset_dict['train'])}, {len(dataloader_dict['train'])}")
 
     # 3) Model loading
     cudnn.benchmark = True
@@ -109,139 +109,100 @@ def augmenting(args):
     start_time_e = time()
     write_log(logger, 'Augmenting start...')
     model.eval()
-    cls_model.eval()
-    seq_list = list()
-    aug_list = dict()
-    epsilon_list = [0, 1, 2, 3, 5, 8, 10]
-    latent_pre_list = list()
-    latent_now_list = list()
-    latent_next_list = list()
+    # cls_model.eval()
+    # seq_list = list()
+    # aug_list = dict()
+    # epsilon_list = [0, 1, 2, 3, 5, 8, 10]
+    # latent_pre_list = list()
+    # latent_now_list = list()
+    # latent_next_list = list()
 
-    for i in epsilon_list:
-        aug_list[f'eps_{i}'] = list()
-        aug_list[f'eps_{i}_prob'] = list()
+    # for i in epsilon_list:
+    #     aug_list[f'eps_{i}'] = list()
+    #     aug_list[f'eps_{i}_prob'] = list()
 
     for i, batch_iter in enumerate(tqdm(dataloader_dict['train'], bar_format='{l_bar}{bar:30}{r_bar}{bar:-2b}')):
 
         # Input setting
         b_iter = input_to_device(batch_iter, device=device)
         src_sequence, src_att, src_seg, trg_label = b_iter
-        trg_label = torch.flip(trg_label, dims=[1])
-        # trg_label = torch.full((len(trg_label), num_labels), 1 / num_labels).to(device)
+        trg_fliped_label = torch.flip(trg_label, dims=[1])
 
+        # Source De-tokenizing
+        origin_source = model.tokenizer.batch_decode(src_sequence, skip_special_tokens=True)
+        origin_list.extend(origin_source)
+        
         # Encoding
         with torch.no_grad():
             encoder_out = model.encode(input_ids=src_sequence, attention_mask=src_att)
-            latent_out = model.latent_encode(input_ids=src_sequence, encoder_out=encoder_out)
+            recon_out = model.generate(encoder_out=encoder_out, attention_mask=src_att, beam_size=args.beam_size,
+                                       beam_alpha=args.beam_alpha, repetition_penalty=args.repetition_penalty, device=device)
+            beam_output = model.tokenizer.batch_decode(recon_out, skip_special_tokens=True)
 
-        # Latent Encoding
-        if model.encoder_out_ratio == 0:
-            latent_out_copy = latent_out.clone().detach().requires_grad_(True)
-            hidden_states_copy = latent_out_copy
-        elif model.latent_out_ratio == 0:
-            encoder_out_copy = encoder_out.clone().detach().requires_grad_(True)
-            hidden_states_copy = encoder_out_copy.max(dim=1)[0]
-        else:
-            encoder_out_copy = encoder_out.clone().detach().requires_grad_(True)
-            latent_out_copy = latent_out.clone().detach().requires_grad_(True)
-            hidden_states = \
-            (model.encoder_out_ratio * encoder_out_copy) + (model.latent_out_ratio * latent_out_copy.unsqueeze(1))
-            hidden_states_copy = hidden_states.clone().detach().requires_grad_(True)
+            inp_dict = model.tokenizer(beam_output,
+                                        max_length=args.src_max_len,
+                                        padding='max_length',
+                                        truncation=True,
+                                        return_tensors='pt')
 
-        with torch.no_grad():
-            recon_out = model(input_ids=src_sequence, attention_mask=src_att, 
-                                hidden_states=hidden_states_copy)
-            origin_output = model.tokenizer.batch_decode(recon_out.argmax(dim=2), skip_special_tokens=True)[0]
+        # Classification for FGSM
+        encoder_out_copy = encoder_out.clone().detach().requires_grad_(True)
+        classifier_out = model.classify(hidden_states=encoder_out_copy)
 
-        classifier_out = model.classify(hidden_states=hidden_states_copy)
-        cls_loss = cls_criterion(classifier_out, trg_label)
+        # Save results
+        aug_dict['eps_0'].extend(beam_output)
+        prob_dict['eps_0'].extend(F.softmax(classifier_out, dim=1))
+
+        # 
+        cls_loss = cls_criterion(classifier_out, trg_fliped_label)
         model.zero_grad()
         cls_loss.backward()
-        hidden_states_copy_grad = hidden_states_copy.grad.data
+        encoder_out_copy_grad = encoder_out_copy.grad.data
+        encoder_out_copy_grad_sign = encoder_out_copy_grad.sign()
 
-        src_output = model.tokenizer.batch_decode(src_sequence, skip_special_tokens=True)
-        seq_list.extend(src_output)
-
-        for epsilon in epsilon_list: # * 0.9
+        for step in range(51): # * 0.9
             
-            hidden_states_copy = hidden_states_copy - ((epsilon * 1) * hidden_states_copy_grad)
-            # hidden_states_copy = ((epsilon * 1) * hidden_states_copy_grad) - hidden_states_copy
+            epsilon = 0.8 # Need to fix
+
+            encoder_out_copy = encoder_out_copy + (epsilon * encoder_out_copy_grad_sign)
 
             with torch.no_grad():
-                recon_out = model.generate(src_input_ids=src_sequence, src_attention_mask=src_att,
-                                                latent_z=hidden_states_copy)
+                recon_out = model.generate(encoder_out=encoder_out_copy, attention_mask=src_att, beam_size=args.beam_size,
+                                        beam_alpha=args.beam_alpha, repetition_penalty=args.repetition_penalty, device=device)
+                beam_output = model.tokenizer.batch_decode(recon_out, skip_special_tokens=True)
 
-            # Augmenting
-            # aug_list[f'eps_{epsilon}'] = model.tokenizer.batch_decode(recon_out.argmax(dim=2), skip_special_tokens=True)[0]  # origin
-            # aug_list[f'eps_{epsilon}'].append(model.tokenizer.batch_decode(recon_out.argmax(dim=2), skip_special_tokens=True)[0]) # one batch
-            aug_list[f'eps_{epsilon}'].extend([i for i in model.tokenizer.batch_decode(recon_out.argmax(dim=2), skip_special_tokens=True)])
-    
-            with torch.no_grad():
-                inp_dict = model.tokenizer(aug_list[f'eps_{epsilon}'][-1],
-                                                max_length=args.src_max_len,
-                                                padding='max_length',
-                                                truncation=True,
-                                                return_tensors='pt')
-                encoder_out = model.encode(input_ids=inp_dict['input_ids'].to(device), attention_mask=inp_dict['attention_mask'].to(device))
-                latent_out = model.latent_encode(input_ids=inp_dict['input_ids'].to(device), encoder_out=encoder_out)
-                
-                # Latent Encoding
-                if model.encoder_out_ratio == 0:
-                    latent_out_copy = latent_out.clone().detach()
-                    hidden_states_copy = latent_out_copy
-                elif model.latent_out_ratio == 0:
-                    encoder_out_copy = encoder_out.clone().detach()
-                    hidden_states_copy = encoder_out_copy.max(dim=1)[0]
-                else:
-                    encoder_out_copy = encoder_out.clone().detach()
-                    latent_out_copy = latent_out.clone().detach()
-                    hidden_states = \
-                    (model.encoder_out_ratio * encoder_out_copy) + (model.latent_out_ratio * latent_out_copy.unsqueeze(1))
-                    hidden_states_copy = hidden_states.clone().detach()
+                inp_dict = model.tokenizer(beam_output,
+                                            max_length=args.src_max_len,
+                                            padding='max_length',
+                                            truncation=True,
+                                            return_tensors='pt')
 
-                if i == 1:
-                    latent_pre_list.append(hidden_states_copy)
-                elif i == 2:
-                    latent_now_list.append(hidden_states_copy)
-                elif i == 3:
-                    latent_next_list.append(hidden_states_copy)
+            # Classification for FGSM
+            encoder_out_copy = encoder_out.clone().detach().requires_grad_(True)
+            classifier_out = model.classify(hidden_states=encoder_out_copy)
 
-                classifier_out = model.classify(hidden_states=hidden_states_copy)
-                aug_list[f'eps_{epsilon}_prob'].extend(F.softmax(classifier_out).tolist())
-        
-        # if i == 3: 
-        #     print([torch.equal(latent_pre_list[i], latent_now_list[i]) for i in range(len(latent_pre_list))])
-        #     print([torch.equal(latent_pre_list[i], latent_next_list[i]) for i in range(len(latent_pre_list))])
-        #     print([torch.equal(latent_now_list[i], latent_next_list[i]) for i in range(len(latent_pre_list))])
+            # Save results
+            if step in [10, 20, 30, 40, 50]:
+                aug_dict[f'eps_{step}'].extend(beam_output)
+                prob_dict[f'eps_{step}'].extend(F.softmax(classifier_out, dim=1))
 
-        # if i == 3:
-        #     break
+            # 
+            cls_loss = cls_criterion(classifier_out, trg_fliped_label)
+            model.zero_grad()
+            cls_loss.backward()
+            encoder_out_copy_grad = encoder_out_copy.grad.data
+            encoder_out_copy_grad_sign = encoder_out_copy_grad.sign()
 
         if args.debuging_mode:
             break
         
-    result_dat = pd.DataFrame({'seq': seq_list})
-    
-    for idx, i in enumerate(epsilon_list):
-            result_dat[f'aug_{idx}'] = aug_list[f'eps_{i}']
-            
-         
-    # result_dat = pd.DataFrame({
-    #     'seq': seq_list,
-    #     'aug_0': aug_list[f'eps_{epsilon_list[0]}'],
-    #     'aug_1': aug_list[f'eps_{epsilon_list[1]}'],
-    #     'aug_2': aug_list['eps_0.5'],
-    #     'aug_3': aug_list['eps_1'],
-    #     'aug_4': aug_list['eps_5'],
-    #     'aug_5': aug_list['eps_10'],
-    #     'aug_6': aug_list['eps_6'],
-    #     'aug_7': aug_list['eps_7'],
-    #     'aug_8': aug_list['eps_8'],
-    #     'aug_9': aug_list['eps_9'],
-    #     'aug_10': aug_list['eps_10']
-    # })
-    result_dat.to_csv(f'./augmenting_result_384_{args.cls_scheduler}.csv', index=False)
+    result_dat = pd.DataFrame({
+        'origin': origin_list,
+        'eps_0': aug_dict['eps_0'],
+        'eps_0_prob': prob_dict['eps_0']
+    })
+    for idx in [10, 20, 30, 40, 50]:
+        result_dat[f'eps_{idx}'] = aug_dict[f'eps_{idx}']
+        result_dat[f'eps_{idx}_prob'] = prob_dict[f'eps_{idx}']
 
-    #===================================#
-    #=========Tokenizing=========#
-    #===================================#
+    result_dat.to_csv(f'./augmenting_result.csv', index=False)
